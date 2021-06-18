0672681b5c919cd79e67fc53b2b9154dc10e8e55
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Pro.local>
2021-06-18 17:47:09 -0300

    layout factory

--- gourmet_game/layout_factory.py
+++ gourmet_game/layout_factory.py
@@ -43,13 +43,3 @@ class LayoutFactory:
 
 
 
-if __name__ == "__main__":
-    factory = LayoutFactory()
-    layout = factory.create_window('TextQuestion', 'Qual comida voce pensou?')
-    janela = simplegui.Window('Tela de inicio', layout)
-    while True:
-        eventos, valores = janela.read()
-        if eventos == simplegui.WINDOW_CLOSED:
-            break
-        if eventos == 'Ok':
-            print(eventos)
